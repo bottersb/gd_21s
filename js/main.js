7e2f278aec2ec2a2a1212db0a4d60fb9c4cc305b
Benjamin Ottersbach <benjamin.ottersbach@gmx.de>
2021-05-05 12:11:26 +0200

    scheduler

--- js/main.js
+++ js/main.js
@@ -1,151 +1,201 @@
+const SEED = 42,
+	RAND = SeedRandom(SEED),
+	MIN_PER_DAY = 24 * 60, // 1440
+	RAD_PER_MIN = (2 * Math.PI) / MIN_PER_DAY; // ~0.004363, 
 
-const font_size=19.2;
-
-
-
-var scene = [
-	{
-		e:lamp,
-		x:-10,
-		y:150,
-		w:300,
-		h:250,
-		f:function(){
-			imageMode(CENTER);
-			image(e,x,y,w,h);
-		}
-	},
-	{
-		e:lamp,
-		x:-10,
-		y:150,
-		w:300,
-		h:250,
-		f:function(){
-			imageMode(CENTER);
-			image(e,x,y,w,h);
-		}
-	},
-	{
-		e:lamp,
-		x:-10,
-		y:150,
-		w:300,
-		h:250,
-		f:function(){
-			imageMode(CENTER);
-			image(e,x,y,w,h);
-		}
-	},
-	{
-		e:lamp,
-		x:-10,
-		y:150,
-		w:300,
-		h:250,
-		f:function(){
-			imageMode(CENTER);
-			image(e,x,y,w,h);
-		}
-	},
-	{
-		e:lamp,
-		x:-10,
-		y:150,
-		w:300,
-		h:250,
-		f:function(){
-			imageMode(CENTER);
-			image(e,x,y,w,h);
-		}
-	},
-	{
-		e:lamp,
-		x:-10,
-		y:150,
-		w:300,
-		h:250,
-		f:function(){
-			imageMode(CENTER);
-			image(e,x,y,w,h);
-		}
-	},
-	{
-		e:lamp,
-		x:-10,
-		y:150,
-		w:300,
-		h:250,
-		f:function(){
-			imageMode(CENTER);
-			image(e,x,y,w,h);
-		}
-	},
-	{
-		e:lamp,
-		x:-10,
-		y:150,
-		w:300,
-		h:250,
-		f:function(){
-			imageMode(CENTER);
-			image(e,x,y,w,h);
-		}
-	},
-	{
-		e:lamp,
-		x:-10,
-		y:150,
-		w:300,
-		h:250,
-		f:function(){
-			imageMode(CENTER);
-			image(e,x,y,w,h);
-		}
-	}
-]
-
-
-
-function drawInterior(){
-	scene.forEach(e => {
-		e.f();
-	});
-	
-	
-	noStroke();
-	
-	// wall
-	fill("SandyBrown");
-	rect(0,0,width,edge);
-	// floor
-	fill("Khaki");
-	rect(0,edge,width,height-edge);
-	// window
-	fill("brown");
-	rect(fenster.x,fenster.y,fenster.w,fenster.h);
-	fill(skyColor);
-	rect(fenster.x+padding,fenster.y+padding,fenster.w-(2*padding),fenster.h-(2*padding));
-
-	//image(lamp,-10,edge-300,  imgSize/3, imgSize/2);
-	image(ball, 3*width/7+10, edge-50, imgSize/9, imgSize/9)
-	image(bed, 30, edge-190);
-	image(bonsai, width/2, edge-200, imgSize/3, imgSize/3);
-	image(desk, 3*width/5+40,edge-190, 4*imgSize/5, 2*imgSize/5);
-	image(chair, 3*width/5,edge-220, imgSize/3, imgSize/2);
-	//image(laptop, 1060,edge-260, imgSize/5, imgSize/8);
-	//image(monitor, 930,edge-430, 3*imgSize/7, 3*imgSize/7);
-	//fill(255);
-	//rect(942,181,250,160);
-	//image(board, 930,edge-430, 3*imgSize/7, 3*imgSize/7);
-	image(shelf, width/2,height/5, imgSize/3, imgSize/4);
-	image(counter, width/11,2*height/3, 3*imgSize/4, imgSize/4);
-	image(wecker, width/11,2*height/3-40, imgSize/9, imgSize/8);
-	
-	// get light, darken room, TODO move to different function
-	let c = color('hsba(180, 100%, 0%,' + 0.80*(1-season(getSunHeight())/100)+ ')');
-	fill(c)
-	//rect(0,0,width, height);
-
-
+const RANDP = function () {
+	return RAND(100) / 100;
 }
+
+const FRAMES_PER_SEC = 60, // when 60 fps, then:
+	TICK = 1000 / FRAMES_PER_SEC, // ~16,667ms
+	FULL_DAY = TICK * MIN_PER_DAY; // 24 seconds irl
+
+// Easing of sun height
+const SUMMER = function (v) { return max(0.1, EasingFunctions.easeOutCubic(v) * 99) },
+	WINTER = function (v) { return max(0.1, EasingFunctions.easeInCubic(v) * 80) },
+	SPRING = function (v) { return max(0.1, EasingFunctions.easeInOutCubic(v) * 90) };
+	FALL = function (v) { return max(0.1, EasingFunctions.easeInOutCubic(v) * 90) };
+
+const col_main = "#40752D",
+	col_main_t70 = "#40752Db3",
+	col_main_dark = "#335E24",
+	col_main_darkest = "#1A2F12",
+	col_main_light = "#5ca840",
+	col_main_lighter = "#D9E3D5",
+	col_main_lightest = "#ECF4E9";
+
+// settings
+var gameSpeed = 1,
+	season = SUMMER,
+	debug = true, // verbose output and display
+	debugUpdateDelay = FRAMES_PER_SEC / 3; // in frames
+
+var speedLimit = true, // fps upper limit to defined value of FRAMES_PER_SEC
+	skipLimit = false, // app may stop bc of background, when false progress will updated according to deltaT 
+	maxFrameSkipAmount = 1; // max amount of frames skipped 
+
+// related, normalized time representations and references
+var frameCounter = 0, // frame counter, integer increased each tick
+	fpsShow = 0, // buffered value for delay	
+	timeCounter = 0, // current day time normalized to distance on unit circle
+	timeCounterRelative = 0, // percent of full day 
+	fpsNow = 0, // true time since last frame may vary and therefore the fps
+	then = Date.now(), // time reference for deltaT
+	deltaT = 0; // progress in ms since last frame
+
+var sunColor = {},
+	skyColor = {};
+
+// fonts
+var fontRegular;
+
+// buttons
+var pause, play, faster, fastest;
+
+// indicators
+var i_energy, i_light, i_melatonin;
+
+const imgSize = 640;
+var edge = 600, padding = 10;
+var bed, bonsai, lamp, desk, ball, shelf, chair, laptop, counter, wecker, monitor, board;
+
+var dailySchedule = [
+	"Sleep",
+	"Sleep",
+	"Sleep",
+	"Sleep",
+	"Sleep",
+	"Sleep",
+	"Sleep",
+	"Recreation",
+	"Recreation",
+	"Work",
+	"Work",
+	"Work",
+	"Work",
+	"Recreation",
+	"Work",
+	"Work",
+	"Work",
+	"Work",
+	"Recreation",
+	"Recreation",
+	"Work",
+	"Work",
+	"Recreation",
+	"Sleep"
+];
+
+
+// compact solution: https://gist.github.com/gre/1650294
+EasingFunctions = {
+	// no easing, no acceleration
+	linear: t => t,
+	// accelerating from zero velocity
+	easeInQuad: t => t * t,
+	// decelerating to zero velocity
+	easeOutQuad: t => t * (2 - t),
+	// acceleration until halfway, then deceleration
+	easeInOutQuad: t => t < .5 ? 2 * t * t : -1 + (4 - 2 * t) * t,
+	// accelerating from zero velocity 
+	easeInCubic: t => t * t * t,
+	// decelerating to zero velocity 
+	easeOutCubic: t => (--t) * t * t + 1,
+	// acceleration until halfway, then deceleration 
+	easeInOutCubic: t => t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1,
+	// accelerating from zero velocity 
+	easeInQuart: t => t * t * t * t,
+	// decelerating to zero velocity 
+	easeOutQuart: t => 1 - (--t) * t * t * t,
+	// acceleration until halfway, then deceleration
+	easeInOutQuart: t => t < .5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t,
+	// accelerating from zero velocity
+	easeInQuint: t => t * t * t * t * t,
+	// decelerating to zero velocity
+	easeOutQuint: t => 1 + (--t) * t * t * t * t,
+	// acceleration until halfway, then deceleration 
+	easeInOutQuint: t => t < .5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t
+}
+
+function SeedRandom(state1, state2) {
+	var mod1 = 4294967087
+	var mul1 = 65539
+	var mod2 = 4294965887
+	var mul2 = 65537
+	if (typeof state1 != "number") {
+		state1 = +new Date()
+	}
+	if (typeof state2 != "number") {
+		state2 = state1
+	}
+	state1 = state1 % (mod1 - 1) + 1
+	state2 = state2 % (mod2 - 1) + 1
+	function random(limit) {
+		state1 = (state1 * mul1) % mod1
+		state2 = (state2 * mul2) % mod2
+		if (state1 < limit && state2 < limit && state1 < mod1 % limit && state2 < mod2 % limit) {
+			return random(limit)
+		}
+		return (state1 + state2) % limit
+	}
+	return random
+}
+
+// interior
+const fenster = {
+	x: 310,
+	y: 350,
+	w: 250,
+	h: 150
+}, i_ball = {
+	x: 600,
+	y: 600,
+	w: 50,
+	h: 50
+}, i_lamp = {
+	x: 300,
+	y: 560,
+	w: 100,
+	h: 140
+}, i_bed = {
+	x: 440,
+	y: 600,
+	w: 400,
+	h: 200
+}, i_bonsai = {
+	x: 700,
+	y: 570,
+	w: 100,
+	h: 100
+}, i_desk = {
+	x: 950,
+	y: 570,
+	w: 240,
+	h: 160
+}, i_chair = {
+	x: 800,
+	y: 600,
+	w: 140,
+	h: 180
+}, i_laptop = {
+	x: 950,
+	y: 480,
+	w: 80,
+	h: 50
+}, i_board = {
+	x: 1130,
+	y: 440,
+	w: 260,
+	h: 190
+}, i_shelf = {
+	x: 680,
+	y: 400,
+	w: 120,
+	h: 80
+}, i_counter = {
+	x: 440,
+	y: 630,
+	w: 300,
+	h: 80
+};
--- js/sketch.js
+++ js/sketch.js
@@ -3,93 +3,6 @@
 // game is designed to run at 1 ingame minute per frame at normal (1x) game speed
 // 
 
-
-const SEED = 42,
-	RAND = SeedRandom(SEED),
-	MIN_PER_DAY = 24 * 60, // 1440
-	RAD_PER_MIN = (2 * Math.PI) / MIN_PER_DAY; // ~0.004363, 
-
-const RANDP = function () {
-	return RAND(100) / 100;
-}
-
-const FRAMES_PER_SEC = 60, // when 60 fps, then:
-	TICK = 1000 / FRAMES_PER_SEC, // ~16,667ms
-	FULL_DAY = TICK * MIN_PER_DAY; // 24 seconds irl
-
-// Easing of sun height
-const SUMMER = function (v) { return max(0.1, EasingFunctions.easeOutCubic(v) * 100) },
-	WINTER = function (v) { return max(0.1, EasingFunctions.easeInCubic(v) * 80) },
-	SPRING = FALL = function (v) { return max(0.1, EasingFunctions.easeInOutCubic(v) * 90) };
-
-const col_main = "#40752D",
-	col_main_t70 = "#40752Db3",
-	col_main_dark = "#335E24",
-	col_main_darkest = "#1A2F12",
-	col_main_light = "#5ca840",
-	col_main_lighter = "#D9E3D5",
-	col_main_lightest = "#ECF4E9";
-
-// settings
-var gameSpeed = 1,
-	season = SUMMER,
-	debug = true, // verbose output and display
-	debugUpdateDelay = FRAMES_PER_SEC / 3; // in frames
-
-var speedLimit = true, // fps upper limit to defined value of FRAMES_PER_SEC
-	skipLimit = false, // app may stop bc of background, when false progress will updated according to deltaT 
-	maxFrameSkipAmount = 1; // max amount of frames skipped 
-
-// related, normalized time representations and references
-var frameCounter = 0, // frame counter, integer increased each tick
-	fpsShow = 0, // buffered value for delay	
-	timeCounter = 0, // current day time normalized to distance on unit circle
-	timeCounterRelative = 0, // percent of full day 
-	fpsNow = 0, // true time since last frame may vary and therefore the fps
-	then = Date.now(), // time reference for deltaT
-	deltaT = 0; // progress in ms since last frame
-
-var sunColor = {},
-	skyColor = {};
-
-// fonts
-var fontRegular;
-
-// buttons
-var pause, play, faster, fastest;
-
-const imgSize = 640;
-var edge = 600, padding = 10;
-var bed, bonsai, lamp, desk, ball, shelf, chair, laptop, counter, wecker, monitor, board;
-
-var daySchedule = [
-	"Sleep",
-	"Sleep",
-	"Sleep",
-	"Sleep",
-	"Sleep",
-	"Sleep",
-	"Sleep",
-	"Recreation",
-	"Recreation",
-	"Work",
-	"Work",
-	"Work",
-	"Work",
-	"Recreation",
-	"Work",
-	"Work",
-	"Work",
-	"Work",
-	"Recreation",
-	"Recreation",
-	"Work",
-	"Work",
-	"Recreation",
-	"Sleep"
-];
-
-
 function preload() {
 	fontRegular = loadFont('res/Poppins-Regular.ttf');
 	lamp = loadImage('img/lamp-575998_640.png');
@@ -104,6 +17,7 @@ function preload() {
 	wecker = loadImage('img/clock-1293099_640.png');
 	monitor = loadImage('img/monitor-2026552_640.png');
 	board = loadImage('img/whiteboard-3205371_640.png');
+
 }
 
 function setup() {
@@ -111,7 +25,9 @@ function setup() {
 	frameRate(FRAMES_PER_SEC);
 
 	loadControls();
+	loadIndicators()
 	loadClock();
+	loadSchedule();
 }
 
 function draw() {
@@ -119,8 +35,9 @@ function draw() {
 	drawSunCicle();
 	//drawAllClouds();
 	drawInterior();
-	drawSchedule();
-	clock();
+	updateClock();
+	darkenRoom(); 
+	//drawSchedule();
 	drawOutline();
 	if (debug) {
 		debugDisplay();
@@ -153,24 +70,6 @@ function loadControls() {
 	fastest.elt.draggable = false;
 	fastest.elt.name = "fastest";
 	fastest.elt.speed = 4;
-}
-
-function loadClock() {
-	let xPos = 800, yPos = 400, dim = 40;
-
-	let divClock = createDiv();
-	divClock.class('clock');
-	let divHour = createSpan();
-	divHour.class('hour');
-	let divMinute = createSpan();
-	divMinute.class('minute');
-	let divDot = createSpan();
-	divDot.class('dot');
-	
-	divHour.parent(divClock);
-	divMinute.parent(divClock);
-	divDot.parent(divClock);
-	divClock.position(xPos, yPos).size(dim,dim);
 }
 
 function controlsEventHandler(event) {
@@ -209,65 +108,6 @@ function drawOutline() {
 	rect(0, height - 30, width, height);
 }
 
-
-// interior
-const fenster = {
-	x: 310,
-	y: 350,
-	w: 250,
-	h: 150
-}, i_ball = {
-	x: 600,
-	y: 600,
-	w: 50,
-	h: 50
-}, i_lamp = {
-	x: 300,
-	y: 560,
-	w: 100,
-	h: 140
-}, i_bed = {
-	x: 440,
-	y: 600,
-	w: 400,
-	h: 200
-}, i_bonsai = {
-	x: 700,
-	y: 570,
-	w: 100,
-	h: 100
-}, i_desk = {
-	x: 950,
-	y: 570,
-	w: 240,
-	h: 160
-}, i_chair = {
-	x: 800,
-	y: 600,
-	w: 140,
-	h: 180
-}, i_laptop = {
-	x: 950,
-	y: 480,
-	w: 80,
-	h: 50
-}, i_board = {
-	x: 1130,
-	y: 440,
-	w: 260,
-	h: 190
-}, i_shelf = {
-	x: 680,
-	y: 400,
-	w: 120,
-	h: 80
-}, i_counter = {
-	x: 440,
-	y: 630,
-	w: 300,
-	h: 80
-}
-
 function drawInterior(){
 	noStroke();
 	// wall
@@ -295,76 +135,123 @@ function drawInterior(){
 	image(counter, i_counter.x, i_counter.y, i_counter.w, i_counter.h);
 }
 
-function drawSchedule(){
+
+
+function loadIndicators(){
+	var xPos = 20, yPos = 180, dim = 50, p = 0;
+
+	i_energy = createImg('img/battery-151574_640.png').position(xPos, yPos + (dim * 1.2 * p++)).size(dim, dim);
+	i_energy.elt.draggable = false;
+
+	i_light = createImg('img/light-156054_640.png').position(xPos, yPos + (dim * 1.2 * p++)).size(dim, dim);
+	i_light.elt.draggable = false;
+
+	i_melatonin = createImg('img/melatonin.png').position(xPos, yPos + (dim * 1.1 * p++)).size(dim, dim);
+	i_melatonin.elt.draggable = false;
+
+}
+
+let divSleepBox, divWorkBox, divRecreationBox;
+function loadSchedule(){
 	var startingX = 200, 
-	startingY = 120, 
-	w = 30,
-	wMargin = 10, 
-	tSchedule = "Schedule: ", 
-	tSleep = "Sleep",
-	cSleep = "MidnightBlue", // RoyalBlue 
-	cSleepHighlight = "RoyalBlue",
-	tWork = "Work",
-	cWork = "Orange",
-	cWorkHighlight = "Gold",
-	tRecreation = "Recreation",
-	cRecreation = "LimeGreen", //Lime
-	cRecreationHighlight = "Lime"; //Lime
-	
-	strokeWeight(0);
-	stroke(0);
-	fill(0);
-	textFont(fontRegular);
-	textSize(w);
-	textAlign(LEFT, BASELINE);
-	text(tSchedule, startingX, startingY-(w/2));
-	
-	var currX = startingX + textWidth(tSchedule) + 40;
-	
-	strokeWeight(1);
-	currX = drawScheduleElement(currX, startingY, w, wMargin, cSleep, cSleepHighlight, tSleep);
-	currX = drawScheduleElement(currX, startingY, w, wMargin, cWork, cWorkHighlight, tWork);
-	drawScheduleElement(currX, startingY, w, wMargin, cRecreation, cRecreationHighlight, tRecreation);
-	
-	textSize(w/2);
-	textAlign(CENTER, CENTER);
-	strokeWeight(1);
-	stroke("black");
-	for(var i = 0; i < 24; i++){
-		switch (daySchedule[i]) {
-			case "Sleep":
-				fill(cSleep);
-			break;
-			case "Work":
-				fill(cWork);
-			break;
-			case "Recreation":
-				fill(cRecreation);
-			break;
-			default:
-				fill(250);
-		}
-		rect(startingX + (i * w), startingY, w, w);
-		fill("white");
-		text(i, startingX + (i * w) + w/2, startingY + w/2);
+    startingY = 120, 
+    w = 30;
+
+	let divScheduleText = createDiv("Schedule:").position(startingX,78);
+	divScheduleText.addClass('scheduleText');
+
+	divSleepBox = createDiv().position(390,80);
+	divSleepBox.addClass('scheduleBox').addClass('sleep').addClass('sleep_selector');
+	divSleepBox.elt.name = "Sleep";
+	divSleepBox.elt.type = "select";
+
+	let divSleepText = createDiv("Sleep").position(390+w+10,78);
+	divSleepText.addClass('scheduleText').addClass('sleep');
+
+	divWorkBox = createDiv().position(525,80);
+	divWorkBox.addClass('scheduleBox').addClass('work').addClass('work_selector');
+	divWorkBox.elt.name = "Work";
+	divWorkBox.elt.type = "select";
+
+	let divWorkText = createDiv("Work").position(525+w+10,78);
+	divWorkText.addClass('scheduleText').addClass('work');
+
+	divRecreationBox = createDiv().position(650,80);
+	divRecreationBox.addClass('scheduleBox').addClass('recreation').addClass('recreation_selector');
+	divRecreationBox.elt.name = "Recreation";
+	divRecreationBox.elt.type = "select";
+
+	let divRecreationText = createDiv("Recreation").position(650+w+10,78);
+	divRecreationText.addClass('scheduleText').addClass('recreation');
+
+	divSleepBox.mouseReleased(scheduleClickListener);
+	divWorkBox.mouseReleased(scheduleClickListener);
+	divRecreationBox.mouseReleased(scheduleClickListener);
+
+	for (let i = 0; i < 24; i++) {
+		let schedType = dailySchedule[i];
+
+		let divScheduleElement = createDiv().position(startingX + (i*w),startingY);
+		divScheduleElement.addClass('scheduleBox').addClass(schedType.toLowerCase());
+		divScheduleElement.elt.scheduleIndex = i;
+		divScheduleElement.elt.type = "put";
+		divScheduleElement.id("schedPut_"+i);
+		divScheduleElement.mouseReleased(scheduleClickListener);
 	}
 }
 
-
-
-function drawScheduleElement(currX, startingY, w, wMargin, c, cH, t){
-	var rectY = startingY - (w+10)
-	if(mouseInside(currX, rectY, w,w)) {
-		fill(cH);
-
+var clickTarget, scheduleTargetName, scheduleInteraction = false, snu;
+function scheduleClickListener(e){
+	// bug related to text inside of the box
+	/*print(e)
+	let el;
+	snu = e;
+	if(e.explicitOriginalTarget.childNodes.length == 0){
+		el = e.explicitOriginalTarget.parentElement;
 	} else {
-		fill(c);
+		el = e.explicitOriginalTarget;
 	}
-	rect(currX, rectY, w, w)
-	currX = currX + w + wMargin;
-	fill(c);
-	text(t, currX, startingY-(w/2));
-	return currX + textWidth(t) + wMargin;
+	print(el)*/
+
+	el = e.explicitOriginalTarget;
+	let left = el.style.left.match(/\d/g).join('');
+	left = parseInt(left,10)
+	let top = el.style.top.match(/\d/g).join('');
+	top = parseInt(top,10)
+	let height = el.clientHeight;
+	let width = el.clientWidth;
+
+	clickTarget = {
+		x: left,
+		y: top,
+		w: width,
+		h: height
+	}
+
+	let type = el.type;
+	if(type == "put") {
+		if(scheduleInteraction) {
+			let element = select('#' + el.id);
+			element.class('').addClass('scheduleBox').addClass(scheduleTargetName.toLowerCase());
+			dailySchedule[el.scheduleIndex] = scheduleTargetName;
+		}
+	} else {
+		//el.classList.add("selected");
+		scheduleTargetName = el.name;
+	}
+}
+
+function mouseReleased() {
+	if(clickTarget !== undefined && mouseInside(clickTarget.x,clickTarget.y,clickTarget.w, clickTarget.h) && scheduleTargetName !== undefined) {
+		scheduleInteraction = true;	
+	} else {
+		scheduleInteraction = false;
+		scheduleTargetName = undefined;
+		/*
+		divSleepBox.elt.classList.remove("selected");
+		divWorkBox.elt.classList.remove("selected");
+		divRecreationBox.elt.classList.remove("selected");*/
+	}
 }
 
 function mouseInside(x, y, w, h){
@@ -373,12 +260,14 @@ function mouseInside(x, y, w, h){
 	} else {
 	 return false; 
 	}
-   }
+}
 
+var currentLight;
 function darkenRoom(){
 	// get light, darken room, TODO move to different function
 	let c = color('hsba(180, 100%, 0%,' + 0.80 * (1 - season(getSunHeight()) / 100) + ')');
-	fill(c)
+	currentLight = c;
+	fill(c);
 	rect(0,0,width, height);
 }
 
@@ -400,19 +289,20 @@ var getSunHeight = function () {
 }
 
 function debugDisplay() {
-	let tSize = 20, p = 5;
+	let tSize = 18, xPos = 1030, yPos = 380, p = 0;
 	strokeWeight(2);
 	stroke(0);
 	fill(200);
 	textFont(fontRegular);
 	textSize(tSize);
 	textAlign(LEFT, BASELINE);
-	text("FPS: " + fpsShow, 10, tSize * p++);
-	text("FNr: " + frameCounter % FRAMES_PER_SEC, 10, tSize * p++);
-	text("d%: " + round(1000 * timeCounterRelative) / 1000, 10, tSize * p++);
-	text("SH: " + round(100 * getSunHeight()) / 100, 10, tSize * p++);
+	text("FPS: " + fpsShow, xPos, yPos + (tSize * p++));
+	text("FNr: " + frameCounter % FRAMES_PER_SEC, xPos, yPos + (tSize * p++));
+	text("d%: " + round(1000 * timeCounterRelative) / 1000, xPos, yPos + (tSize * p++));
+	text("SH: " + round(100 * getSunHeight()) / 100, xPos, yPos + (tSize * p++));
+	text("S: " + season.name, xPos, yPos + (tSize * p++));
 
-	print(mouseX, mouseY);
+	//print(mouseX, mouseY);
 }
 
 function updateDelta() {
@@ -443,62 +333,26 @@ function deltaTLimiter() {
 	}
 }
 
-// compact solution: https://gist.github.com/gre/1650294
-EasingFunctions = {
-	// no easing, no acceleration
-	linear: t => t,
-	// accelerating from zero velocity
-	easeInQuad: t => t * t,
-	// decelerating to zero velocity
-	easeOutQuad: t => t * (2 - t),
-	// acceleration until halfway, then deceleration
-	easeInOutQuad: t => t < .5 ? 2 * t * t : -1 + (4 - 2 * t) * t,
-	// accelerating from zero velocity 
-	easeInCubic: t => t * t * t,
-	// decelerating to zero velocity 
-	easeOutCubic: t => (--t) * t * t + 1,
-	// acceleration until halfway, then deceleration 
-	easeInOutCubic: t => t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1,
-	// accelerating from zero velocity 
-	easeInQuart: t => t * t * t * t,
-	// decelerating to zero velocity 
-	easeOutQuart: t => 1 - (--t) * t * t * t,
-	// acceleration until halfway, then deceleration
-	easeInOutQuart: t => t < .5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t,
-	// accelerating from zero velocity
-	easeInQuint: t => t * t * t * t * t,
-	// decelerating to zero velocity
-	easeOutQuint: t => 1 + (--t) * t * t * t * t,
-	// acceleration until halfway, then deceleration 
-	easeInOutQuint: t => t < .5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t
+function loadClock() {
+	let xPos = 800, yPos = 400, dim = 40;
+
+	let divClock = createDiv();
+	divClock.class('clock');
+	let divHour = createSpan();
+	divHour.class('hour');
+	let divMinute = createSpan();
+	divMinute.class('minute');
+	let divDot = createSpan();
+	divDot.class('dot');
+	
+	divHour.parent(divClock);
+	divMinute.parent(divClock);
+	divDot.parent(divClock);
+	divClock.position(xPos, yPos).size(dim,dim);
 }
 
-function SeedRandom(state1, state2) {
-	var mod1 = 4294967087
-	var mul1 = 65539
-	var mod2 = 4294965887
-	var mul2 = 65537
-	if (typeof state1 != "number") {
-		state1 = +new Date()
-	}
-	if (typeof state2 != "number") {
-		state2 = state1
-	}
-	state1 = state1 % (mod1 - 1) + 1
-	state2 = state2 % (mod2 - 1) + 1
-	function random(limit) {
-		state1 = (state1 * mul1) % mod1
-		state2 = (state2 * mul2) % mod2
-		if (state1 < limit && state2 < limit && state1 < mod1 % limit && state2 < mod2 % limit) {
-			return random(limit)
-		}
-		return (state1 + state2) % limit
-	}
-	return random
-}
-
-function clock() {
-	var currentMins = floor(map(timeCounterRelative, 0,1,0,1440));
+function updateClock() {
+	var currentMins = floor(map(timeCounterRelative,0,1,0,1440));
 	const hour = ((floor(currentMins / 60) + 11) % 12 + 1) * 30;
 	const minute = floor(currentMins % 60) * 6;
 	document.querySelector('.hour').style.transform = `rotate(${hour}deg)`;
