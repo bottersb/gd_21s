7293f018d7fed1a0d811d44ff7c723a5d67c9d7e
Benjamin Ottersbach <benjamin.ottersbach@gmx.de>
2021-05-17 19:15:11 +0200

    speed control via keyboard

--- js/sketch.js
+++ js/sketch.js
@@ -36,8 +36,9 @@ function draw() {
 	//drawAllClouds();
 	drawInterior();
 	updateClock();
-	darkenRoom(); 
+	darkenRoom();
 	//drawSchedule();
+	drawIndicators();
 	drawOutline();
 	if (debug) {
 		debugDisplay();
@@ -57,7 +58,6 @@ function loadControls() {
 	play.elt.draggable = false;
 	play.elt.name = "play";
 	play.elt.speed = 1;
-	play.elt.classList.toggle("selected");
 
 	faster = createImg('img/faster.png').position(xPos + (dim * 1.5 * p++), 100).size(dim, dim);
 	faster.mouseOver(controlsEventHandler).mouseOut(controlsEventHandler).mouseReleased(controlsEventHandler);
@@ -70,6 +70,8 @@ function loadControls() {
 	fastest.elt.draggable = false;
 	fastest.elt.name = "fastest";
 	fastest.elt.speed = 4;
+
+	play.elt.classList.toggle("selected");
 }
 
 function controlsEventHandler(event) {
@@ -81,6 +83,7 @@ function controlsEventHandler(event) {
 			event.target.classList.toggle("shadow");
 			break;
 		case 'mouseup':
+			lastGameSpeed = undefined;
 			pause.elt.classList.remove("selected");
 			play.elt.classList.remove("selected");
 			faster.elt.classList.remove("selected");
@@ -88,6 +91,35 @@ function controlsEventHandler(event) {
 			event.target.classList.toggle("selected");
 			gameSpeed = event.target.speed;
 			break;
+	}
+}
+
+let lastGameSpeed;
+function keyReleased() {
+	if (keyCode === 32) {
+		pause.elt.classList.remove("selected");
+		play.elt.classList.remove("selected");
+		faster.elt.classList.remove("selected");
+		fastest.elt.classList.remove("selected");
+		if (lastGameSpeed == undefined) {
+			lastGameSpeed = gameSpeed;
+			pause.elt.classList.toggle("selected");
+			gameSpeed = 0;
+		} else {
+			gameSpeed = lastGameSpeed;
+			switch(lastGameSpeed){
+				case 1:
+					play.elt.classList.toggle("selected");	
+					break;
+				case 2:
+					faster.elt.classList.toggle("selected");
+					break;
+				case 4:
+					fastest.elt.classList.toggle("selected");
+					break;
+			}
+			lastGameSpeed = undefined;
+		}
 	}
 }
 
@@ -108,7 +140,8 @@ function drawOutline() {
 	rect(0, height - 30, width, height);
 }
 
-function drawInterior(){
+let testi;
+function drawInterior() {
 	noStroke();
 	// wall
 	fill("SandyBrown");
@@ -123,65 +156,98 @@ function drawInterior(){
 	rect(fenster.x + padding, fenster.y + padding, fenster.w - (2 * padding), fenster.h - (2 * padding));
 
 	imageMode(CENTER);
-	image(lamp, i_lamp.x, i_lamp.y,i_lamp.w,i_lamp.h)
-	image(ball, i_ball.x, i_ball.y,i_ball.w,i_ball.h)
-	image(bed, i_bed.x, i_bed.y,i_bed.w,i_bed.h)
-	image(bonsai, i_bonsai.x, i_bonsai.y,i_bonsai.w,i_bonsai.h)
-	image(desk, i_desk.x, i_desk.y,i_desk.w,i_desk.h)
-	image(chair, i_chair.x, i_chair.y,i_chair.w,i_chair.h)
-	image(laptop, i_laptop.x, i_laptop.y,i_laptop.w,i_laptop.h)
+	image(lamp, i_lamp.x, i_lamp.y, i_lamp.w, i_lamp.h)
+	image(ball, i_ball.x, i_ball.y, i_ball.w, i_ball.h)
+	image(bed, i_bed.x, i_bed.y, i_bed.w, i_bed.h)
+	image(bonsai, i_bonsai.x, i_bonsai.y, i_bonsai.w, i_bonsai.h)
+	image(desk, i_desk.x, i_desk.y, i_desk.w, i_desk.h)
+	image(chair, i_chair.x, i_chair.y, i_chair.w, i_chair.h)
+	image(laptop, i_laptop.x, i_laptop.y, i_laptop.w, i_laptop.h)
 	image(board, i_board.x, i_board.y, i_board.w, i_board.h);
 	image(shelf, i_shelf.x, i_shelf.y, i_shelf.w, i_shelf.h);
 	image(counter, i_counter.x, i_counter.y, i_counter.w, i_counter.h);
 }
 
-
-
-function loadIndicators(){
+function loadIndicators() {
 	var xPos = 20, yPos = 180, dim = 50, p = 0;
-
-	i_energy = createImg('img/battery-151574_640.png').position(xPos, yPos + (dim * 1.2 * p++)).size(dim, dim);
-	i_energy.elt.draggable = false;
 
 	i_light = createImg('img/light-156054_640.png').position(xPos, yPos + (dim * 1.2 * p++)).size(dim, dim);
 	i_light.elt.draggable = false;
 
-	i_melatonin = createImg('img/melatonin.png').position(xPos, yPos + (dim * 1.1 * p++)).size(dim, dim);
+	i_melatonin = createImg('img/melatonin.png').position(xPos, yPos + (dim * 1.2 * p++)).size(dim, dim);
 	i_melatonin.elt.draggable = false;
+
+	i_energy = createImg('img/battery-151574_640.png').position(xPos, yPos + (dim * 1.2 * p++)).size(dim, dim);
+	i_energy.elt.draggable = false;
+}
+
+let brightness = 0, currMelatonin = 0.4;
+function drawIndicators() {
+	var xPos = 20, yPos = 180, dim = 50, maxW = 150, p = 0;
+	let energyBar, melatoninBar, light;
+
+	noStroke();
+	fill(250);
+
+	var startingX = 200,
+		startingY = 120,
+		w = 30;
+
+	rect(map(timeCounterRelative, 0, 1, startingX, startingX + (24 * w)), startingY + w, 5, 10);
+
+	stroke(1)
+	strokeWeight(1)
+	fill(250);
+	rect(xPos + dim + 10, yPos + (dim * 1.2 * p++) + 10, maxW, dim - 20);
+	rect(xPos + dim + 10, yPos + (dim * 1.2 * p++) + 10, maxW, dim - 20);
+	rect(xPos + dim + 10, yPos + (dim * 1.2 * p++) + 10, maxW, dim - 20);
+	p = 0;
+	fill("yellow");
+	brightness = 0.90 * season(getSunHeight());
+	rect(xPos + dim + 10, yPos + (dim * 1.2 * p++) + 10, map(brightness, 0, 100, 0, maxW), dim - 20);
+	fill("cyan");
+	var mD = map(brightness, 0, 100, 0.005, -0.005);
+	if (gameSpeed != 0) {
+		currMelatonin += mD;
+		currMelatonin = min(1, currMelatonin);
+		currMelatonin = max(0, currMelatonin);
+	}
+	rect(xPos + dim + 10, yPos + (dim * 1.2 * p++) + 10, map(currMelatonin, 0, 1, 0, maxW), dim - 20);
+
 
 }
 
 let divSleepBox, divWorkBox, divRecreationBox;
-function loadSchedule(){
-	var startingX = 200, 
-    startingY = 120, 
-    w = 30;
+function loadSchedule() {
+	var startingX = 200,
+		startingY = 120,
+		w = 30;
 
-	let divScheduleText = createDiv("Schedule:").position(startingX,78);
+	let divScheduleText = createDiv("Schedule:").position(startingX, 78);
 	divScheduleText.addClass('scheduleText');
 
-	divSleepBox = createDiv().position(390,80);
+	divSleepBox = createDiv().position(390, 80);
 	divSleepBox.addClass('scheduleBox').addClass('sleep').addClass('sleep_selector');
 	divSleepBox.elt.name = "Sleep";
 	divSleepBox.elt.type = "select";
 
-	let divSleepText = createDiv("Sleep").position(390+w+10,78);
+	let divSleepText = createDiv("Sleep").position(390 + w + 10, 78);
 	divSleepText.addClass('scheduleText').addClass('sleep');
 
-	divWorkBox = createDiv().position(525,80);
+	divWorkBox = createDiv().position(525, 80);
 	divWorkBox.addClass('scheduleBox').addClass('work').addClass('work_selector');
 	divWorkBox.elt.name = "Work";
 	divWorkBox.elt.type = "select";
 
-	let divWorkText = createDiv("Work").position(525+w+10,78);
+	let divWorkText = createDiv("Work").position(525 + w + 10, 78);
 	divWorkText.addClass('scheduleText').addClass('work');
 
-	divRecreationBox = createDiv().position(650,80);
+	divRecreationBox = createDiv().position(650, 80);
 	divRecreationBox.addClass('scheduleBox').addClass('recreation').addClass('recreation_selector');
 	divRecreationBox.elt.name = "Recreation";
 	divRecreationBox.elt.type = "select";
 
-	let divRecreationText = createDiv("Recreation").position(650+w+10,78);
+	let divRecreationText = createDiv("Recreation").position(650 + w + 10, 78);
 	divRecreationText.addClass('scheduleText').addClass('recreation');
 
 	divSleepBox.mouseReleased(scheduleClickListener);
@@ -191,17 +257,17 @@ function loadSchedule(){
 	for (let i = 0; i < 24; i++) {
 		let schedType = dailySchedule[i];
 
-		let divScheduleElement = createDiv().position(startingX + (i*w),startingY);
+		let divScheduleElement = createDiv().position(startingX + (i * w), startingY);
 		divScheduleElement.addClass('scheduleBox').addClass(schedType.toLowerCase());
 		divScheduleElement.elt.scheduleIndex = i;
 		divScheduleElement.elt.type = "put";
-		divScheduleElement.id("schedPut_"+i);
+		divScheduleElement.id("schedPut_" + i);
 		divScheduleElement.mouseReleased(scheduleClickListener);
 	}
 }
 
 var clickTarget, scheduleTargetName, scheduleInteraction = false, snu;
-function scheduleClickListener(e){
+function scheduleClickListener(e) {
 	// bug related to text inside of the box
 	/*print(e)
 	let el;
@@ -215,9 +281,9 @@ function scheduleClickListener(e){
 
 	el = e.explicitOriginalTarget;
 	let left = el.style.left.match(/\d/g).join('');
-	left = parseInt(left,10)
+	left = parseInt(left, 10)
 	let top = el.style.top.match(/\d/g).join('');
-	top = parseInt(top,10)
+	top = parseInt(top, 10)
 	let height = el.clientHeight;
 	let width = el.clientWidth;
 
@@ -229,8 +295,8 @@ function scheduleClickListener(e){
 	}
 
 	let type = el.type;
-	if(type == "put") {
-		if(scheduleInteraction) {
+	if (type == "put") {
+		if (scheduleInteraction) {
 			let element = select('#' + el.id);
 			element.class('').addClass('scheduleBox').addClass(scheduleTargetName.toLowerCase());
 			dailySchedule[el.scheduleIndex] = scheduleTargetName;
@@ -242,8 +308,8 @@ function scheduleClickListener(e){
 }
 
 function mouseReleased() {
-	if(clickTarget !== undefined && mouseInside(clickTarget.x,clickTarget.y,clickTarget.w, clickTarget.h) && scheduleTargetName !== undefined) {
-		scheduleInteraction = true;	
+	if (clickTarget !== undefined && mouseInside(clickTarget.x, clickTarget.y, clickTarget.w, clickTarget.h) && scheduleTargetName !== undefined) {
+		scheduleInteraction = true;
 	} else {
 		scheduleInteraction = false;
 		scheduleTargetName = undefined;
@@ -254,21 +320,21 @@ function mouseReleased() {
 	}
 }
 
-function mouseInside(x, y, w, h){
-	if(mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h) {
-	 return true; 
+function mouseInside(x, y, w, h) {
+	if (mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h) {
+		return true;
 	} else {
-	 return false; 
+		return false;
 	}
 }
 
 var currentLight;
-function darkenRoom(){
+function darkenRoom() {
 	// get light, darken room, TODO move to different function
 	let c = color('hsba(180, 100%, 0%,' + 0.80 * (1 - season(getSunHeight()) / 100) + ')');
 	currentLight = c;
 	fill(c);
-	rect(0,0,width, height);
+	rect(0, 0, width, height);
 }
 
 function drawSunCicle() {
@@ -344,15 +410,15 @@ function loadClock() {
 	divMinute.class('minute');
 	let divDot = createSpan();
 	divDot.class('dot');
-	
+
 	divHour.parent(divClock);
 	divMinute.parent(divClock);
 	divDot.parent(divClock);
-	divClock.position(xPos, yPos).size(dim,dim);
+	divClock.position(xPos, yPos).size(dim, dim);
 }
 
 function updateClock() {
-	var currentMins = floor(map(timeCounterRelative,0,1,0,1440));
+	var currentMins = floor(map(timeCounterRelative, 0, 1, 0, 1440));
 	const hour = ((floor(currentMins / 60) + 11) % 12 + 1) * 30;
 	const minute = floor(currentMins % 60) * 6;
 	document.querySelector('.hour').style.transform = `rotate(${hour}deg)`;
